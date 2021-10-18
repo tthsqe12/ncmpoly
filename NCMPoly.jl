8f1619ea06de192e651a023485bad95997063a1e
Julien Schanz <s8juscha@stud.uni-saarland.de>
2021-10-18 13:00:32 +0200

    implemented 3 of 4 cases for removing redundant obstructions

--- NCMPoly.jl
+++ NCMPoly.jl
@@ -175,6 +175,14 @@ function FreeAssociativeAlgebra(R::Ring, s::Vector{Symbol}; cached::Bool = true)
    return (parent_obj, gens(parent_obj))
 end
 
+function deg(a::NCMPoly{T}) where T
+        if a.length > 0
+                return length(leading_word(a))
+        else
+                return 0
+        end
+end
+
 function leading_coefficient(a::NCMPoly{T}) where T
    @assert a.length > 0
    return a.coeffs[1]
@@ -569,30 +577,197 @@ function obstructions(a::Vector{Int})
    return res   
 end
 
-# theorem 4.1.14
-function buchberger(g::Vector{NCMPoly{T}}) where T
 
-println("**** starting buchberge on ****")
-show(stdout, "text/plain", g)
-println()
+# check whether w_2 = v w_1 for some word v
+function issubwordright(w_1::Vector{Int}, w_2::Vector{Int})
+        if length(w_1) > length(w_2)
+                return false
+        end
+        for i in 0:length(w_1) - 1
+                if w_1[length(w_1) - i] != w_2[length(w_2) - i]
+                        return false
+                end
+        end
+        return true
+end
 
-   g = copy(g)
-   R = parent(g[1])
-   dummy_obstr = (Int[], Int[], Int[], Int[])
-   empty_obstr_set = typeof(dummy_obstr)[]
 
-# step 1
-   s = length(g)
-   B = typeof(empty_obstr_set)[
-            if i > j
+# check whether w_2 = w_1 v for some word v
+function issubwordleft(w_1::Vector{Int}, w_2::Vector{Int})
+        if length(w_1) > length(w_2)
+                return false
+        end
+
+        for i in 1:length(w_1)
+                if w_1[i] != w_2[i]
+                        return false
+                end
+        end
+        return true
+end
+
+
+###
+# check if for obs1 = (w_i, w_i^'; u_j, u_j^') and obs2 = (w_k, w_k^'; v_l, v_l^') 
+# it holds that u_j == w v_l and u_j^' = v_l^' w^' for some w, w^'
+# i.e. if obs2 is a subobstruction of obs1
+# both w and w^' might be empty
+function issubobstruction(obs1::NTuple{4, Vector{Int}}, obs2::NTuple{4, Vector{Int}})
+#        if length(obs2[3]) > length(obs1[3]) || length(obs2[4]) > length(obs1[4])
+#                return false
+#        end
+        if issubwordright(obs2[3], obs1[3]) && issubwordleft(obs2[4], obs1[4]) 
+                return true
+        else
+                return false
+        end
+end
+
+# check whether there exists a w^'' such that
+# w1 LM(g1) w2 = w1 LM(g1) w^'' LM(g2) u2
+function hasoverlap(g1, g2, w1, w2, u1, u2)
+        lw1 = leading_word(g1)
+        lw2 = leading_word(g2)
+        concatenated_word = vcat(w1, lw1, w2)
+        for i in 1:length(w1)
+                c = popfirst!(concatenated_word)
+                @assert c == w1[i]
+        end
+        for i in 1:length(lw1)
+                c = popfirst!(concatenated_word)
+                @assert c == lw1[i]
+        end
+        for j in 0:length(u2)-1
+                c = pop!(concatenated_word)
+                @assert c = u2[length(u2)-j]
+        end
+        if length(concatenated_word) < length(lw2)
+                return false
+        end
+        return issubwordright(lw2, concatenated_word) # TODO maybe just comparing lengths should be sufficient
+end
+
+function isredundant(obs::NTuple{4, Vector{Int}}, obs_index::Int, s::Int, B::Matrix{Vector{NTuple{4, Vector{Int}}}})
+        # cases 4b + 4c
+        for j in 1:size(B)[1]
+                for k in 1:length(B[j, s])
+                        if issubobstruction(obs, B[j, s][k])
+                                # case 4b
+                                if length(obs[3]) - length(B[j, s][k][3]) + length(obs[4]) - length(B[j,s][k][4]) > 0
+                                        return true
+                                # case 4c        
+                                elseif obs_index > j 
+                                        return true
+                                elseif obs_index == j && length(obs[3]) - length(B[j, s][k][3]) + length(obs[4]) - length(B[j,s][k][4]) == 0 && monomial_gt(obs[1], B[j, s][k][1])
+                                        return true
+                                end
+                        end
+                end
+        end
+        # case 4d
+        # size(B) should be (s, s)
+        # we want to iterate over all B[i, obs_index] with i < s
+        for i in 1:size(B)[1]-1 
+                for k in 1:length(B[i, obs_index])
+                        if issubwordright(B[i, obs_index][k][3], obs[1]) && issubwordleft(B[i, obs_index][k][4], obs[2])
+                                show(obs)
+                                show(B[i, obs_index][k])
+                                u1 = copy(obs[1])
+                                u2 = copy(obs[2])
+                                v1 = copy(B[i, obs_index][k][3])
+                                v2 = copy(B[i, obs_index][k][4])
+                                for i in 1:length(v1)
+                                        pop!(u1)
+                                end
+                                for j in 1:length(v2)
+                                        popfirst!(u2)
+                                end
+                                @assert monomial_cmp(vcat(u1, v1), obs[1]) == 0
+                                @assert monomial_cmp(vcat(v2, u2), obs[2]) == 0
+                                if !hasoverlap(g[i], g[s], vcat(u1, B[i, obs_index][k][1]), vcat(B[i, obs_index][k][2], u2), obs[3], obs[4]) 
+                                        return true
+                                end
+                        end
+                end
+        end
+        return false
+end
+
+## s is the index of the newly added layer of obstructions in B
+function remove_redundancies!(B::Matrix{Vector{NTuple{4, Vector{Int}}}}, s::Int, g::Vector{NCMPoly{T}}) where T
+        del_counter = 0
+        for i in 1:s
+                k = 1
+                while k <= length(B[i, s])
+                        if isredundant(B[i, s][k], i, s, B)
+                                deleteat(B[i, j][k])
+                                del_counter += 1
+                        else 
+                                k += 1
+                        end
+                end
+        end
+        # TODO case 4e from Thm 4.1 in Kreuzer Xiu
+end
+
+function get_obstructions(g::Vector{NCMPoly{T}}, s::Int) where T
+        dummy_obstr = (Int[], Int[], Int[], Int[])
+        empty_obstr_set = typeof(dummy_obstr)[]
+        new_B = typeof(empty_obstr_set)[
+                    if i > j
+                       empty_obstr_set
+                    elseif i == j
+                       obstructions(leading_word(g[i]))
+                    else
+                       obstructions(leading_word(g[i]), leading_word(g[j]))
+                    end
+                 for i in 1:s, j in 1:s]
+        obstr_count = 0
+        for i in 1:s, j in 1:s
+                obstr_count += length(new_B[i,j])
+        end
+        # TODO maybe here some redundancies can be removed too, check Kreuzer Xiu
+        println("$obstr_count many obstructions")
+        return new_B
+
+end
+
+
+function add_obstructions(g::Vector{NCMPoly{T}}, B::Matrix{Vector{NTuple{4, Vector{Int64}}}}, s::Int) where T
+        dummy_obstr = (Int[], Int[], Int[], Int[])
+        empty_obstr_set = typeof(dummy_obstr)[]
+        new_B = Vector{NTuple{4, Vector{Int64}}}[
+            if i < s && j < s
+               # copy old entry
+               B[i, j]
+            elseif i > j
                empty_obstr_set
             elseif i == j
                obstructions(leading_word(g[i]))
             else
                obstructions(leading_word(g[i]), leading_word(g[j]))
             end
-         for i in 1:s, j in 1:s]
+            for i in 1:s, j in 1:s]
+        remove_redundancies!(new_B, s, g)
+        return new_B
+end
 
+
+# theorem 4.1.14
+function buchberger(g::Vector{NCMPoly{T}}) where T
+
+println("**** starting buchberger ****")
+#show(stdout, "text/plain", g)
+println()
+
+   g = copy(g)
+   R = parent(g[1])
+   
+# step 1
+   s = length(g)
+   dummy_obstr = (Int[], Int[], Int[], Int[])
+   empty_obstr_set = typeof(dummy_obstr)[]
+   B = get_obstructions(g, s)
 @label step2
    @assert s == length(g)
    i = j = 0
@@ -612,15 +787,15 @@ println("\ndone!")
    end
 
 # step3
-println("\nobstruction ", (i, j), ": ", R(o[1]), ", ", R(o[2]),
-                                  "; ", R(o[3]), ", ", R(o[4]))
-@show g[i]
-@show g[j]
+#println("\nobstruction ", (i, j), ": ", R(o[1]), ", ", R(o[2]),
+#                                  "; ", R(o[3]), ", ", R(o[4]))
+#@show g[i]
+#@show g[j]
    S = mul_term(inv(leading_coefficient(g[i])), o[1], g[i], o[2]) -
        mul_term(inv(leading_coefficient(g[j])), o[3], g[j], o[4])
-@show S
+#@show S
    Sp = rem(S, g) # or rem_weak
-@show Sp
+#@show Sp
    if iszero(Sp)
       @goto step2
    end
@@ -629,20 +804,72 @@ println("\nobstruction ", (i, j), ": ", R(o[1]), ", ", R(o[2]),
    s += 1
    push!(g, Sp)
    # julia doesn't have a matrix resize!, so make a new matrix :(
-   B = typeof(empty_obstr_set)[
-            if i < s && j < s
-               # copy old entry
-               B[i, j]
-            elseif i > j
-               empty_obstr_set
-            elseif i == j
-               obstructions(leading_word(g[i]))
-            else
-               obstructions(leading_word(g[i]), leading_word(g[j]))
-            end
-         for i in 1:s, j in 1:s]
+   B = add_obstructions(g, B, s)
    @goto step2
 end
+
+
+
+function buchberger(g::Vector{NCMPoly{T}}, degbound::Int) where T
+
+println("**** starting buchberger ****")
+#show(stdout, "text/plain", g)
+println()
+
+   g = copy(g)
+   R = parent(g[1])
+   checked_obstructions = 0
+# step 1
+   s = length(g)
+   dummy_obstr = (Int[], Int[], Int[], Int[])
+   empty_obstr_set = typeof(dummy_obstr)[]
+
+   B = get_obstructions(g, s)
+ @label step2
+   @assert s == length(g)
+   i = j = 0
+   o = dummy_obstr
+   # check all entries with i <= j
+   for jj in 1:s, ii in 1:jj
+      if !isempty(B[ii, jj])
+         (i, j) = (ii, jj)
+         o = popfirst!(B[i, j])
+         break
+      end
+   end
+   if !(i > 0 && j > 0)
+println("\ndone!")
+      # B is empty
+      return g
+   end
+
+# step3
+#println("\nobstruction ", (i, j), ": ", R(o[1]), ", ", R(o[2]),
+#                                  "; ", R(o[3]), ", ", R(o[4]))
+#@show g[i]
+#@show g[j]
+   S = mul_term(inv(leading_coefficient(g[i])), o[1], g[i], o[2]) -
+       mul_term(inv(leading_coefficient(g[j])), o[3], g[j], o[4])
+#@show S
+   Sp = rem(S, g) # or rem_weak
+   checked_obstructions += 1
+   if checked_obstructions % 5000 == 0
+           println("checked $checked_obstructions obstructions")
+   end
+#@show Sp
+   if iszero(Sp) || deg(Sp) > degbound
+      @goto step2
+   end
+
+# step4
+   s += 1
+   push!(g, Sp)
+   # julia doesn't have a matrix resize!, so make a new matrix :(
+   println("adding new obstructions! checked $checked_obstructions so far")
+   B = add_obstructions(g, B, s)
+   @goto step2
+end
+
 
 ###############################################################################
 #
@@ -668,10 +895,10 @@ debug_obstructions(Int[1,2,1])
 # example 4.1.15 continued:
 #  typo1: the last member of O(1,2) is o_{1,2}(1, (x*y)^2*x*t; u*(x*y)^2*x, 1)
 #  typo2: g2 = u*(x*y)^3 + u*(x*y)^2 + u + v
-R, (x, y, u, v, t, s) = FreeAssociativeAlgebra(GF(2), ["x", "y", "u", "v", "t", "s"])
-g = buchberger([u*(x*y)^3 + u*(x*y)^2 + u + v,
-                (y*x)^3*t + (y*x)^2*t + t + s])
-show(stdout, "text/plain", g)
-println()
+#R, (x, y, u, v, t, s) = FreeAssociativeAlgebra(GF(2), ["x", "y", "u", "v", "t", "s"])
+#g = buchberger([u*(x*y)^3 + u*(x*y)^2 + u + v,
+#                (y*x)^3*t + (y*x)^2*t + t + s])
+#show(stdout, "text/plain", g)
+#println()
 
 nothing
--- graph_to_free_alg.jl
+++ graph_to_free_alg.jl
@@ -2,46 +2,53 @@ include("NCMPoly.jl")
 
 
 function free_alg_for_graph(basering::Ring, adj::Matrix{Int})
-   (n, ncols) = size(adj)
-   @assert n == ncols
-   generator_strings = String[]
-   for i in 1:n, j in 1:n
-      push!(generator_strings, "u[$i,$j]")
-   end
-   A, g = FreeAssociativeAlgebra(basering, generator_strings)
-   u = Matrix{elem_type(A)}(undef, n, n)
-   for i in 1:n, j in 1:n
-      u[i, j] = g[(i-1)*size(adj)[2] + j]
-   end
-   relations = elem_type(A)[]
-   for i in 1:n, j in 1:n
-      push!(relations, u[i, j] * u[i, j] - u[i, j])
-      for k in 1:n
-         if k != j
-            push!(relations, u[i, j]*u[i, k])
-            push!(relations, u[j, i]*u[k, i])
-         end
-         for l in 1:n
-            if adj[i, k] != adj[j, l]
-               push!(relations, u[i, j]*u[k, l])
-               push!(relations, u[k, l]*u[i, j])
-            end
-         end
-      end
-   end
-   sum_1 = zero(A)
-   sum_2 = zero(A)
-   for i in 1:n
-      for k in 1:n
-         sum_1 += u[i, k]
-         sum_2 += u[k, i]
-      end
-      push!(relations, sum_1 - one(A))
-      push!(relations, sum_2 - one(A))
-      sum_1 = zero(A)
-      sum_2 = zero(A)
-   end
-   return A, u, relations
+        (n, ncols) = size(adj)
+        @assert n == ncols
+        generator_strings = String[]
+        for i in 1:n, j in 1:n
+                push!(generator_strings, "u[$i,$j]")
+        end
+        A, g = FreeAssociativeAlgebra(basering, generator_strings)
+        u = Matrix{elem_type(A)}(undef, n, n)
+        for i in 1:n, j in 1:n
+                u[i, j] = g[(i-1)*size(adj)[2] + j]
+        end
+        relations = elem_type(A)[]
+        for i in 1:n, j in 1:n
+                push!(relations, u[i, j] * u[i, j] - u[i, j])
+                for k in 1:n
+                        if k != j
+                                if !(u[i,j]*u[i,k] in relations)
+                                        push!(relations, u[i, j]*u[i, k])
+                                end
+                                if !(u[j,i]*u[k,i] in relations)
+                                        push!(relations, u[j, i]*u[k, i])
+                                end
+                                for l in 1:n
+                                        if adj[i, k] != adj[j, l] && !( u[i, j]*u[k, l] in relations)
+                                                push!(relations, u[i, j]*u[k, l])
+                                        end
+                                end
+                        end
+                end
+        end
+        sum_1 = zero(A)
+        sum_2 = zero(A)
+        for i in 1:n
+                for k in 1:n
+                        sum_1 += u[i, k]
+                        sum_2 += u[k, i]
+                end
+                if !(sum_1 - one(A) in relations)
+                        push!(relations, sum_1 - one(A))
+                end
+                if !(sum_2 - one(A) in relations)
+                        push!(relations, sum_2 - one(A))
+                end
+                sum_1 = zero(A)
+                sum_2 = zero(A)
+        end
+        return A, u, relations
 end
 
 function check_commutativity(u::Matrix{NCMPoly{Rational{BigInt}}}, gb::Vector{NCMPoly{Rational{BigInt}}}, A::NCMPolyRing{Rational{BigInt}})
@@ -49,7 +56,7 @@ function check_commutativity(u::Matrix{NCMPoly{Rational{BigInt}}}, gb::Vector{NC
                 for j in 1:size(u)[2]
                         for k in 1:size(u)[1]
                                 for l in 1:size(u)[2]
-                                        if iszero(rem(u[i, j]*u[k, l] - u[k, l] * u[i, j], gb))
+                                        if !iszero(rem(u[i, j]*u[k, l] - u[k, l] * u[i, j], gb))
                                                 return false
                                         end
                                 end
